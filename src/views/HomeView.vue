56edc5f9a18ed95d0004a4d740dc9b62dab43578
Sabrina <hsif0111@gmail.com>
2024-01-05 08:08:08 +0800

    提交測試

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,11 +9,5 @@ import ElementsPage from '../components/elementsPage.vue'
 </script>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
